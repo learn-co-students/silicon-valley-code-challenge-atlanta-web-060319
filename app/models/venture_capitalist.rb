3e9d7e10d2472586734cc78643588c71af67b973
github username <github email address>
2019-06-14 07:31:22 -0400

    done

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,45 @@
 class VentureCapitalist
 
+    attr_accessor :name, :total_worth
+
+    @@capitalists = []
+
+    def initialize (name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@capitalists << self
+    end
+
+    def self.all
+        @@capitalists
+    end
+
+    def self.tres_commas_club
+        VentureCapitalist.all.select {|capitalist| capitalist.total_worth > 1000000000}
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end 
+
+    def funding_rounds
+        FundingRound.all.select {|rounds| rounds.venture_capitalist == self}
+    end
+
+    def portfolio
+        self.funding_rounds.collect {|rounds| round.startup}
+    end
+
+    def biggest_investment
+        self.funding_rounds.max_ by{|round| round.investment} 
+    end
+
+    def invested(domain)
+        x = funding_rounds.collect do |round|
+          round.startup.domain == domain
+          round.investment
+        end
+        x.sum
+      end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,9 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/funding_round.rb'
+require_relative '../app/models/startup.rb'
+require_relative '../app/models/venture_capitalist.rb'
+
+require 'pry'
 
 def reload
   load 'config/environment.rb'
@@ -6,6 +11,22 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+
+hop = Startup.new("Hop", "Josh Riser", "hop.com")
+drop = Startup.new("Drop", "Kate Mitchell", "drop.com")
+roll = Startup.new("Roll", "Bill Jones", "roll.com")
+
+bill = VentureCapitalist.new("Bill Gates", 10000000000000)
+frank = VentureCapitalist.new("Frank Harris", 5000000000)
+jane = VentureCapitalist.new("Jane Doe", 999999999)
+
+
+round_1 = FundingRound.new(hop, frank, "Seed", 100000)
+round_5 = FundingRound.new(hop, bill, "Pre Seed", 30000)
+round_6 = FundingRound.new(hop, jane, "Seed", 50000)
+round_7 = FundingRound.new(hop, bill, "Seed", 40000)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
